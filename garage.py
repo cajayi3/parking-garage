a0c4fada58c18f0bd864f3e9555874ebd090d2c1
Christopher Ajayi <christopher@codingtemple.com>
2023-07-27 03:29:35 -0500

    first commit

--- garage.py
+++ garage.py
@@ -1,49 +1,38 @@
-class Car:
-    def __init__(self, licensePlate):
-        self.licensePlate = licensePlate
+tickets = [10, 23, 14, 18, 12]
+parkingSpaces = ['3a', '4b', '7f', '8h', '3e']
+currentTicket = { 15.00:"Paid", 0.00:"Not Paid"}
+
+class ParkingGarage:
+
+    def __init__(self, tickets: int = 5):
+        self.tickets = [i for i in range (tickets)]
+        self.parkingSpaces = [i for i in range(tickets)]
+        self.currentTicket = {}
+        self.PayforParking = []
         
-
-class ParkingLot:
-    def __init__(self, row, lane, level, car):
-        self.row = row
-        self.lane = lane
-        self.level = level
-        self.car = car
-
-    def Available(self):
-        self.car = None
-    def park(self, vehicle):
-        self.car = vehicle
-
-    def removeVehicle(self):
-        self.car = None
-
-class Level:
-    def __init__(self, rows, levelNumber):
-        self.levelNumber = levelNumber
-        self.rows =rows
-        self.lotPerRow = 2
-        self.parkingLot = []
-        self.available = rows * self.lotsPerRow
-    
-    def findAvailableLot(self):
-        if self.AvailableLot <= 0:
-            return None
+    def takeTicket(self):
+        ticket_number = self.tickets.pop()
+        parking_number = self.parkingSpaces.pop()
+        self.active_tickets.insert(0,"ticket"+ str(ticket_number))
+        self.currentTicket["ticket"+ str(ticket_number)] = 'Occupied'
+        print(f"Your ticket number is {ticket_number}, your parking number is {parking_number} ")
+      
+    def PayforParking(self):
+        parkingLot = input("Need to make a Payment?:")
+        if len(parkingLot) >= 1:
+            return (f"{self.ticket} has been Paid, you have 15 minutes to exist:")
         else:
-            if(len(self.parkingLot) == 0):
-                return ParkingLot(0,0,0,None)
-            else:
-                lastCarParked = self.parkingLot[-1]
-            if(lastCarParked.LotNumber<self.lotPerRow):
-                return ParkingLot(lastCarParked.row, lastCarParked.lotNumber+1,self.levelNumber,None)
-            if(lastCarParked.row<self.rows):
-                return ParkingLot(lastCarParked.row+1,0,self.levelNumber, None)
-            
-            def park(self, vehicle):
-                freeParkingLot = self.findAvailableLot()
-                if(not(freeParkingLot)):
-                    return False
-                    
-                else:
-                    freeParkingLot.park(vehicle)
-                    self.parkingLot.park(vehicle)
+            while len(parkingLot) < 1:
+                parkingLot = input("Please make a current payment of 15 dollars:")
+
+    def leaveGarage(self):
+        if self.currentTicket["ticket"+str(len(self.tickets))] == "Paid":
+            print("Thank you, have a marvelous day!:")
+            self.tickets.append(len(self.tickets))
+            self.parkingSpaces.append(len(self.parkingSpaces))
+
+if __name__ == '__main__':
+    customer1 = ParkingGarage(15.00, 'Troy', 'Robinson', '12', '4b')
+    customer2 = ParkingGarage(15.00, 'Michael','Thompson', '10', '8h')
+    customer3 = ParkingGarage(0.00, 'Monica', 'Jones', '14', '7f')
+
